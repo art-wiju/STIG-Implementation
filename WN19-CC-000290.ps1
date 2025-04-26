4433ef7761b6e6f76e0c95c1b4a93d47efdaf4cf
Wilson Arteaga <132944565+art-wiju@users.noreply.github.com>
2025-04-25 19:40:38 -0500

    Create WN19-CC-000290.ps1

---
+++ WN19-CC-000290.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+    This PowerShell script configures the event log size to 32768 KB or greater. It is set up between 1-20 MB by default. Inadequate log size will cause the log to fill up quickly. This may prevent audit events from being recorded properly and require frequent attention by administrative personnel.
+
+.NOTES
+    Author          : Wilson Arteaga
+    LinkedIn        : linkedin.com/in/wilson-arteaga-v/
+    GitHub          : github.com/art-wiju
+    Date Created    : 2025-04-25
+    Last Modified   : 2025-04-25
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN19-CC-000290
+
+.TESTED ON
+    Date(s) Tested  : 2025-04-25
+    Tested By       : Wilson Arteaga
+    Systems Tested  : Windows Server 2019 Datacenter
+    PowerShell Ver. : 5.1.17763.7240
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# Define the registry path and value
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\System"
+$valueName = "MaxSize"
+$valueData = 32768
+
+# Create the registry key if it doesn't exist
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force
+}
+
+# Set the registry value
+Set-ItemProperty -Path $registryPath -Name $valueName -Value $valueData -Type DWord
+
+Write-Output "Registry key and value have been set successfully."
