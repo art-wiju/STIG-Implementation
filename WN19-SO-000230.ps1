a3ea965008521a5d751336f015e3c28a375583c1
Wilson Arteaga <132944565+art-wiju@users.noreply.github.com>
2025-04-25 19:45:55 -0500

    Create WN19-SO-000230.ps1

---
+++ WN19-SO-000230.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+    This PowerShell script blocks anonymous enumeration of shares. Allowing anonymous logon users (null session connections) to list all account names and enumerate (perform reconnaissance) all shared resources can provide a map of potential points to attack the system.
+
+.NOTES
+    Author          : Wilson Arteaga
+    LinkedIn        : linkedin.com/in/wilson-arteaga-v/
+    GitHub          : github.com/art-wiju
+    Date Created    : 2025-04-25
+    Last Modified   : 2025-04-25
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN19-SO-000230
+
+.TESTED ON
+    Date(s) Tested  : 2025-04-25
+    Tested By       : Wilson Arteaga
+    Systems Tested  : Windows Server 2019 Datacenter
+    PowerShell Ver. : 5.1.17763.7240
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# Define the registry path and value
+$registryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa"
+$valueName = "RestrictAnonymous"
+$valueData = 1
+
+# Create the registry key if it doesn't exist
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force
+}
+
+# Set the registry value
+Set-ItemProperty -Path $registryPath -Name $valueName -Value $valueData -Type DWord
+
+Write-Output "Registry key and value have been set successfully."
